25febf8764e5465be6bdd2be0df6bfeb8c552921
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-13 22:23:38 +0200

    styled Header

---
+++ src/sections/Header/Header.styles.tsx
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const headerStyles = css`
+display: flex;
+justify-content: space-around;
+padding: 10px;
+`
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -3,13 +3,14 @@
 import { LanguageSwitcher } from "@/components";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { headerStyles } from "./Header.styles";
 
 export const Header = () => {
       const params = useParams();
       const locale = params?.locale as string;
 
   return (
-    <header>
+    <header css={headerStyles}>
       <Link href="/">Home</Link>
       <Link href={`/${locale}/products`}>Products</Link>
       <LanguageSwitcher />
