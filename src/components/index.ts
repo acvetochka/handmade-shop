9756c17151e053c80d3c594354221aeac2dc9272
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-15 19:00:08 +0200

    add Container to HeroSection

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from "./CategoryCard/CategoryCard";
+export * from "./CategoryLink/CategoryLink";
+export * from "./Container/Container";
 export * from "./LanguageSwitcher/LanguageSwitcher";
