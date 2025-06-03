13d42dabd2fba62ddcbf7e1d083a690a298235f9
Alona Kuznietsova <acvetochka@gmail.com>
2025-06-03 22:59:23 +0200

    add container

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,12 @@
+// export const Product = (product) => {
+// return (
+//     <div>
+//              <img
+//                 src={`http://localhost:1337${product.images[0].url}`}
+//                 alt={product.title}
+//                 width={200}
+//               />
+        
+//     </div>
+// )
+// }
