7d5e4e350ea96ead69eff813725d7bfeb936d54f
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-08 22:08:32 +0200

    update Image into product-page

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,23 +1,24 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
-import { fetchProducts } from '@/lib/strapi-api';
-import { useTranslation } from '@/i18n/I18nProvider';
-import { productItem, productList } from '@/styles/ProductsPage.styles';
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchProducts } from "@/lib/strapi-api";
+import { useTranslation } from "@/i18n/I18nProvider";
+import { productItem, productList } from "@/styles/ProductsPage.styles";
+import Image from "next/image";
 
 export default function ProductsPage() {
   const params = useParams();
   const locale = params?.locale as string;
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   const [products, setProducts] = useState<any[]>([]);
 
   useEffect(() => {
     fetchProducts(locale).then(setProducts);
-  }, [locale]);  
-  
+  }, [locale]);
+
   return (
     <section>
       <h1>{t("products.pageTitle")}</h1>
@@ -26,13 +27,15 @@ export default function ProductsPage() {
           <li key={product.id} css={productItem}>
             <h2>{product.title}</h2>
             {product.images[0] && (
-              <img
+              <Image
                 src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${product.images[0].url}`}
                 alt={product.title}
                 width={200}
               />
             )}
-            <p>{t("products.price")}: {product.price} €</p>
+            <p>
+              {t("products.price")}: {product.price} €
+            </p>
           </li>
         ))}
       </ul>
