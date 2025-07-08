0d541493bdbbd00f3ceec4ca0d93de3c087038fe
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-08 21:57:18 +0200

    move Container to ui

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export * from "./CategoryCard/CategoryCard";
 export * from "./CategoryLink/CategoryLink";
-export * from "./Container/Container";
+export * from "./ui/Container/Container";
 export * from "./LanguageSwitcher/LanguageSwitcher";
