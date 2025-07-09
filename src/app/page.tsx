3843ccdfa1d116f6e016c19448f680bb88a0f704
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-09 10:09:13 +0200

    clear code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,8 @@ import { redirect } from "next/navigation";
 import { defaultLocale, locales } from "@/lib/i18n";
 
 export default async function RootPage() {
-  // const cookieStore =
-  //   cookies() as import("next/headers").ReadonlyRequestCookies;
-  // const preferredLocale = cookieStore.get("preferred-locale")?.value;
-
   const cookieStore = await cookies();
   const preferredLocale = cookieStore.get("preferred-locale")?.value;
-
-  // const locale = locales.includes(preferredLocale)
-  //   ? preferredLocale
-  //   : defaultLocale;
 
   const locale =
     preferredLocale &&
