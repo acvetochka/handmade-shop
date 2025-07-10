4e88d4af14d53c3520e5f69e06a6b5bffaf7e2fc
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-10 23:37:07 +0200

    update Container component

--- src/components/ui/Container/Container.styles.tsx
+++ src/components/ui/Container/Container.styles.tsx
@@ -1,19 +1,24 @@
 import { css } from "@emotion/react";
 
 export const container = css`
-  padding: 0 60px;
+  padding: 0 20px;
   text-align: center;
   // max-width: 1440px;
-  max-width: 393px
+  max-width: 393px;
   margin: 0 auto;
   width: 100%;
 
-  @media screen and (min-width: 393px){
-  max-width: 393px}
+  @media screen and (min-width: 393px) {
+    max-width: 393px;
+  }
 
-   @media screen and (min-width: 768px){
-  max-width: 768px}
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+    padding: 0 40px;
+  }
 
-     @media screen and (min-width: 1440px){
-  max-width: 1440px}
+  @media screen and (min-width: 1440px) {
+    max-width: 1440px;
+    padding: 0 60px;
+  }
 `;
