68feb874ac4443daf81310fc2b998e3268bafe6e
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-10 23:35:52 +0200

    add ContactSection

--- src/sections/ContactSection/ContactSection.styles.tsx
+++ src/sections/ContactSection/ContactSection.styles.tsx
@@ -6,3 +6,39 @@ export const contactSection = css`
   padding: 40px 0;
   //  : "#dfd5d5"};
 `;
+
+export const contactWrapper = css`
+  @media (min-width: 768px) {
+    align-items: center;
+  }
+  @media (min-width: 1020px) {
+    flex-direction: row;
+    align-items: flex-start;
+  }
+
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const listStyled = css`
+  list-style: none;
+  padding-left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  // align-items: center;
+  width: 300px;
+`;
+
+export const itemStyled = css`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+
+  & p {
+    text-transform: capitalize;
+    width: 70px;
+  }
+`;
--- src/sections/ContactSection/ContactSection.tsx
+++ src/sections/ContactSection/ContactSection.tsx
@@ -2,14 +2,34 @@
 
 import { Container } from "@/components";
 import { ContactForm } from "@/components/ContactForm/ContactForm";
-import { contactSection } from "./ContactSection.styles";
+import {
+  contactSection,
+  contactWrapper,
+  itemStyled,
+  listStyled,
+} from "./ContactSection.styles";
 import { JSX } from "react";
+import contacts from "@/data/contacts.json";
 
 export const ContactSection = (): JSX.Element => {
   return (
-    <section id="contacts" css={contactSection}>
+    <section id="kontakte" css={contactSection}>
       <Container>
-        <ContactForm />
+        <div css={contactWrapper}>
+          <ul css={listStyled}>
+            {contacts.map(({ name, type, link }, id) => (
+              <li css={itemStyled} key={id}>
+                <p>{type}:</p>
+                <a href={link}>{name}</a>
+              </li>
+            ))}
+            <li css={itemStyled}>
+              <p>Location:</p>
+              <span>Germany, Bavaria</span>
+            </li>
+          </ul>
+          <ContactForm />
+        </div>
       </Container>
     </section>
   );
