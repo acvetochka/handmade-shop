ad2a542ffda019ae63f3e9ee64df1431c42d72da
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-13 21:25:03 +0200

    change HeroSection

--- src/sections/HeroSection/HeroSection.styles.tsx
+++ src/sections/HeroSection/HeroSection.styles.tsx
@@ -20,3 +20,60 @@ export const title = css`
     font-size: 72px;
   }
 `;
+
+export const warning = css`
+  padding: 20px;
+  border: 1px solid red;
+  margin-bottom: 20px;
+`;
+
+export const linkStyled = css`
+  position: relative;
+  max-width: 150px;
+  padding: 12px 24px;
+  border: none;
+  background: linear-gradient(
+    to right,
+    var(--button-hover) 50%,
+    var(--button-background) 50%
+  );
+  background-size: 200% 100%;
+  background-position: right bottom;
+  border-radius: 8px;
+  color: var(--foreground);
+  transition: background-position 0.4s ease;
+  cursor: pointer;
+  font-size: 16px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  margin-top: 20px;
+
+  & p {
+    transition: transform 0.3s ease;
+    display: inline-block;
+  }
+
+  & span {
+    position: absolute;
+    right: 10px;
+    opacity: 0;
+    transform: translateX(-10px);
+    transition: all 0.3s ease;
+    pointer-events: none;
+  }
+
+  &:hover {
+    background-position: left bottom;
+
+    & p {
+      transform: translateX(-10px);
+    }
+
+    & span {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+`;
--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Container } from "@/components";
+import { Container, ScrollLink } from "@/components";
 import { useTranslation } from "@/i18n/I18nProvider";
-import { heroSection, title } from "./HeroSection.styles";
+import { heroSection, linkStyled, title, warning } from "./HeroSection.styles";
 import { JSX } from "react";
+// import { categoryButton } from "@/components/CategoryLink/CategoryLink.styles";
 
 export const HeroSection = (): JSX.Element => {
   const { t } = useTranslation();
@@ -11,6 +12,17 @@ export const HeroSection = (): JSX.Element => {
   return (
     <section css={heroSection}>
       <Container>
+        <div css={warning}>
+          <p>{t("home.warning")}</p>
+          {/* <p>
+            Die Website befindet sich aktuell im Aufbau und wird bis zum 16.Juli
+            2025 fertiggestellt. Sie können mich jedoch bereits jetzt über die
+            Kontaktdaten am Ende der Seite erreichen
+          </p> */}
+          <ScrollLink to="kontakte" css={linkStyled}>
+            <p css={linkStyled}>Kontakte</p>
+          </ScrollLink>
+        </div>
         <h1 css={title}>{t("home.title")}</h1>
         <p>{t("home.description")}</p>
       </Container>
