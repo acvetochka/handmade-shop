ef659a9aa057891927e76caaa49afcd22b668dad
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-16 14:03:34 +0200

    add images

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,9 +1,69 @@
 // ⚠️ Auto-generated file. Do not edit manually.
 
+export const threeDPrintImages = [
+  "/images/threeDPrint/photo_2025-07-15_22-18-11 (2).jpg",
+  "/images/threeDPrint/photo_2025-07-15_22-18-11 (3).jpg",
+  "/images/threeDPrint/photo_2025-07-15_22-18-11.jpg",
+  "/images/threeDPrint/photo_2025-07-15_22-18-14 (5).jpg",
+  "/images/threeDPrint/photo_2025-07-15_22-18-14.jpg",
+];
+
+export const candlesImages = [
+  "/images/candles/photo_2025-07-15_22-18-11 (2).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (2).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (3).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (4).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (5).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (6).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12 (7).jpg",
+  "/images/candles/photo_2025-07-15_22-18-12.jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (2).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (3).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (4).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (5).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (6).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13 (7).jpg",
+  "/images/candles/photo_2025-07-15_22-18-13.jpg",
+  "/images/candles/photo_2025-07-15_22-18-14 (2).jpg",
+  "/images/candles/photo_2025-07-15_22-18-14 (3).jpg",
+  "/images/candles/photo_2025-07-15_22-18-14 (4).jpg",
+  "/images/candles/photo_2025-07-15_22-18-14 (7).jpg",
+  "/images/candles/photo_2025-07-15_22-18-14.jpg",
+];
+
 export const cardsImages = [
   "/images/cards/01.jpg",
   "/images/cards/02.jpg",
   "/images/cards/03.jpg",
   "/images/cards/04.jpg",
-  "/images/cards/05.jpg"
+  "/images/cards/05.jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (10).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (2).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (3).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (4).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (5).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (6).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (7).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (8).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10 (9).jpg",
+  "/images/cards/photo_2025-07-15_22-18-10.jpg",
+  "/images/cards/photo_2025-07-15_22-18-11 (2).jpg",
+  "/images/cards/photo_2025-07-15_22-18-11.jpg",
+];
+
+export const gipsImages = [
+  "/images/gips/photo_2025-07-15_22-18-11 (4).jpg",
+  "/images/gips/photo_2025-07-15_22-18-11.jpg",
+  "/images/gips/photo_2025-07-15_22-18-12.jpg",
+  "/images/gips/photo_2025-07-15_22-18-13 (2).jpg",
+  "/images/gips/photo_2025-07-15_22-18-13 (3).jpg",
+  "/images/gips/photo_2025-07-15_22-18-13 (4).jpg",
+  "/images/gips/photo_2025-07-15_22-18-13 (8).jpg",
+  "/images/gips/photo_2025-07-15_22-18-13.jpg",
+  "/images/gips/photo_2025-07-15_22-18-14 (2).jpg",
+  "/images/gips/photo_2025-07-15_22-18-14 (3).jpg",
+  "/images/gips/photo_2025-07-15_22-18-14 (4).jpg",
+  "/images/gips/photo_2025-07-15_22-18-14 (5).jpg",
+  "/images/gips/photo_2025-07-15_22-18-14 (6).jpg",
+  "/images/gips/photo_2025-07-15_22-18-14.jpg",
 ];
