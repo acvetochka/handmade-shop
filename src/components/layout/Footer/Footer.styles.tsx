5088ab1b4f09bd3ff60db7ccffa6d268806643d3
Alona Kuznietsova <acvetochka@gmail.com>
2025-09-30 22:06:19 +0200

    change footer width

--- src/components/layout/Footer/Footer.styles.tsx
+++ src/components/layout/Footer/Footer.styles.tsx
@@ -6,7 +6,7 @@ export const footerStyles = css`
   background-color: rgb(183 157 157 / 95%);
   box-shadow: 0 1px 10px 0px #311704;
   border-radius: 8px 8px 0 0;
-  max-width: 393px;
+  // max-width: 393px;
   margin: 0 auto;
   width: 100%;
   // height: 60px;
@@ -18,17 +18,17 @@ export const footerStyles = css`
   margin-top: auto;
 
   @media screen and (min-width: 393px) {
-    max-width: 393px;
+    // max-width: 393px;
   }
 
   @media screen and (min-width: 768px) {
-    max-width: 768px;
+    // max-width: 768px;
     padding: 10px 40px;
     flex-direction: row;
   }
 
   @media screen and (min-width: 1440px) {
-    max-width: 1440px;
+    // max-width: 1440px;
     padding: 10px 60px;
   }
 `;
